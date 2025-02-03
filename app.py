78be7eaab6b63196cd9c7af1b4aa9863342d2700
Vignesh R <rvikky1994@gmail.com>
2025-02-03 10:20:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ app.route("/order_products/<int:order_product_id>", methods=["DELETE"])(delete_o
 
 # ========================= RUN FLASK SERVER =========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
